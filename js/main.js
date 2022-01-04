94d93bff932809a5554c9eb12d5903f982fbc9ab
Abhishek Tiwari <abhishek.tiwari3507@gmail.com>
2022-01-04 17:11:37 +0530

    update

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
     "use strict";
 
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
+        scrollDuration : 800, // x duration
         mailChimpURL   : ''   // mailchimp url
     },
 
